eac7a2202ac2f6bdf0850f53b12b426c95c4c84c
evaldo503 <evaldo503@gmail.com>
2013-07-29 16:44:52 -0300

    Update TesteGit.java

--- TesteGit/src/testegit/TesteGit.java
+++ TesteGit/src/testegit/TesteGit.java
@@ -14,6 +14,7 @@ public class TesteGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        lol
         // TODO code application logic here
     }
 }
